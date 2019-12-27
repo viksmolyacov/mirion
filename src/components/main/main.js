6ffae6336815943ec4ef65596b8af95ff1302e3b
viksmolyacov <viktor.sm93@gmail.com>
2019-12-27 16:29:24 +0700

    fix main styles

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './main.css';
-import Logo from '../../images/logo.png';
+// import Logo from '../../images/logo.png';
 import Shirt1 from '../../images/shirt1.png';
 import Shirt2 from '../../images/shirt2.png';
 import Shirt3 from '../../images/shirt3.png';
